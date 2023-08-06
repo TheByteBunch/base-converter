6bee2991aa63e8f2a1ceaf468c2d2cb94812f9a5
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-08-06 03:40:06 -0500

    sketch out validation logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from choosing import choose_from_base, choose_to_base, choose_input_number
+from validating import validate_input_number
 from converting import convert_base
 
 bases = [2, 8, 10, 16]
@@ -7,7 +8,10 @@ from_base = choose_from_base(bases)
 
 to_base = choose_to_base(from_base, bases)
 
-input_number = choose_input_number(from_base)
+valid_input_number = False
+while not valid_input_number:
+    input_number = choose_input_number(from_base)
+    valid_input_number = validate_input_number(from_base, input_number)
 
 output_number = convert_base(from_base, to_base, input_number)
 
