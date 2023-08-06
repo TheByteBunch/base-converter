0fd94ef7c3dfa8bbd09112dc0f1a27820c6c38ab
Navid <navidjalili3@gmail.com>
2023-08-06 19:43:33 +0330

    Update main

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from sys import path
+from src.cli.prompt import select_base
+from src.utils.validator import Validator
+from src.core.convert import convert_base
 
 
 def main():
-    pass
+    origin_base = select_base()
+    entered_number = input("Enter number:")
+
+    Validator.validate_input(entered_number, origin_base)
+
+    destination_base = select_base()
+
+    converted_number = convert_base(
+        entered_number, origin_base, destination_base)
+    print(converted_number)
 
 
 if __name__ == "__main__":
